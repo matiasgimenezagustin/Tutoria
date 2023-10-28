484d4512d468fabe1fba7aa4e360e602518f4d8a
matu.dev <matu.a.gimenez2002@gmail.com>
2023-10-27 22:07:38 -0300

    clase 2 tutoria

---
+++ clase-2/script.js
@@ -0,0 +1,69 @@
+/* Un lenguaje de programacion  
+
+JS ES UN LENGUAJE CASE SENSITIVE
+*/
+
+
+/* Tipos de datos primitivos: */
+
+
+/* 
+
+string 
+number
+boolean
+undefined
+null
+
+*/
+
+/* 
+Number
+
+4
+3.4
+-5
+Infinity
+NaN 
+*/
+
+
+
+console.log(typeof NaN)//NOT A NUMBER
+
+
+/* 
+STRING
+
+'hola3'
+
+'null'
+
+"pepe"
+
+`juan`
+*/
+
+console.log("tv samsung 40\"")
+/* 
+BOOLEAN 
+true false
+*/
+
+/* 
+
+undefined
+null
+*/
+
+/* COMPARADORES SIEMPRE devuelven booleanos*/
+
+//IGUALDAD
+console.log(2 == true)
+
+console.log(0 == false)
+
+console.log('1' == 1)
+
+//ESTRICTA IGUALDAD
+console.log(1 === 2)
